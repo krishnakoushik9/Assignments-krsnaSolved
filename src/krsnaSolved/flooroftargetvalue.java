453a704271b63a142c7d781a315df2772eb70c09
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-10 15:13:55 +0530

    Floor of a target number

---
+++ src/krsnaSolved/flooroftargetvalue.java
@@ -0,0 +1,30 @@
+package krsnaSolved;
+
+public class flooroftargetvalue {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 7;
+        int ans = searchInsert(arr,target);
+        System.out.println(ans);
+    }
+    public static int searchInsert(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return arr[mid];
+            }
+        }
+        if (start <= arr.length - 1) {
+            return arr[end];
+        }
+        return -1;
+    }
+
+}
