87a9e9ac9bcf4aee49ecf1db8c4b1829491c4db6
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-12 21:47:47 +0530

    cyclic Sort finding the missing number

---
+++ src/krsnaSolved/findMissingValue.java
@@ -0,0 +1,31 @@
+package krsnaSolved;
+
+public class findMissingValue {
+    public static void main(String[] args) {
+        int[] arr = {3,1,0};
+        int ans = missingNumber(arr);
+        System.out.println(ans);
+    }
+        public static int missingNumber(int[] arr){
+            int i = 0;
+            int n = arr.length;
+            while(i < n){
+                int correct = arr[i];
+                if(arr[i]< n && arr[i] != arr[correct]){
+                    swap(arr,i,correct);
+                }else{
+                    i++;
+                }
+            }
+            for (int j = 0;j < n;j++){
+                if(arr[j]!=j){
+                    return j;
+                }
+            }return n;
+        }
+        public static void swap(int[] arr, int a, int b){
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+}
