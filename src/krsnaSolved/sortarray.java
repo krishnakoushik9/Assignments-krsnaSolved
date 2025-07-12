8403bc6d6701b7264861fbe90d8902d9a41a12ba
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-12 14:16:26 +0530

    Bubble sort

--- src/krsnaSolved/sortarray.java
+++ src/krsnaSolved/sortarray.java
@@ -1,7 +1,7 @@
 package krsnaSolved;
 
 import java.util.Arrays;
-
+//Bubble Sort
 public class sortarray {
     public static void main(String[] args) {
         int[] arr = {7,2,82,42,12,57,92,24,38,1};
@@ -10,19 +10,17 @@ public class sortarray {
     }
 
     static void staticresponce(int[] arr) {
-        for (int pass = 0; pass < arr.length - 1; pass++) {
-            int i = 0;
-            while (i < arr.length - 1) {
-                int temp;
-                if (arr[i] > arr[i + 1]) {
-                    temp = arr[i + 1];
-                    arr[i + 1] = arr[i];
-                    arr[i] = temp;
+        boolean swapped;
+        for(int i = 0 ;i<arr.length;i++){
+            swapped = false;
+            for(int j = 1;j <  arr.length - i;j++){
+                if(arr[j]<arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
                 }
-                i++;
-            }
+            }if (!swapped)break;
         }
     }
-
-
 }
