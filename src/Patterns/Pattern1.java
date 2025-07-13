ebddf2d5aac06d64b3f691204147d7c39457cb3d
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-13 21:11:50 +0530

    Patterns 1

---
+++ src/Patterns/Pattern1.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern1(6);
+
+    }
+    static void pattern1(int n){
+        for(int row = 1;row<=n;row++){
+            for(int col = 1;col<=n;col++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
