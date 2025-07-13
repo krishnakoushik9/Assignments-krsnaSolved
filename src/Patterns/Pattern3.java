5bf70c89796650fc37737dbde2257a30dd7848bc
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-13 21:17:57 +0530

    Pattern 3

---
+++ src/Patterns/Pattern3.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        pattern3(6);
+    }
+    static void pattern3(int n){
+        for(int row = 1;row<=n;row++){
+            for(int col = n;col>=row;col--){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
