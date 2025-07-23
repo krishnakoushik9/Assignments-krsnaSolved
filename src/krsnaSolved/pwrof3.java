ac36ecdca9aff00aa1a6f12da573aa613a75916f
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-23 14:40:01 +0530

    Commit

--- src/krsnaSolved/pwrof3.java
+++ src/krsnaSolved/pwrof3.java
@@ -6,7 +6,7 @@ public class pwrof3 {
         boolean ans = sqr3(n);
         System.out.println(ans);
     }
-
+//Testing for Push to Git
     private static boolean sqr3(int n) {
         if(n<=0)return false;
         while(n % 3 == 0){
