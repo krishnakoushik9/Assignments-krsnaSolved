abe79d52a44ef8238fabce11c95b8394611a798e
krishnakoushik9 <legionkoushik3@gmail.com>
2025-10-26 22:21:59 +0530

    Pattern7.java it's a reverse triangle but written in recursion

--- src/Patterns/Revisit/Pattern7.java
+++ src/Patterns/Revisit/Pattern7.java
@@ -14,6 +14,19 @@ public class Pattern7 {
             trainglePrint(r-1,0);
         }
     }
+    public static void traingleNormal(int r,int c){
+        if(r == 0){
+            return;
+        }
+        if (c < r){
+            traingleNormal(r,c+1);
+            System.out.printf("*");
+        }
+        else{
+            traingleNormal(r-1,0);
+            System.out.println();
+        }
+    }
     //Main
     public static void main(String[] args) {
         // * * * *
@@ -21,5 +34,10 @@ public class Pattern7 {
         // * *
         // *
         trainglePrint(4,0);
+        // *
+        // * *
+        // * * *
+        // * * * *
+        traingleNormal(4,0);
     }
 }
